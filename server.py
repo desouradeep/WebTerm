7a2232ebdd51be8d8d03bad1b5c3f9f74aba0de2
Souradeep De <souradeep.2011@gmail.com>
2014-10-20 17:46:34 +0530

    Add minimal backend server

---
+++ server.py
@@ -0,0 +1,90 @@
+from gevent import monkey
+import gevent
+import json
+
+import subprocess
+from socketio import socketio_manage
+from socketio.server import SocketIOServer
+from socketio.namespace import BaseNamespace
+from socketio.mixins import BroadcastMixin, RoomsMixin
+
+monkey.patch_all()
+
+
+class EnrouteNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
+
+    def recv_connect(self):
+        def notify():
+            while True:
+                self.broadcast_event(
+                    'data',
+                    hello(),
+                )
+                gevent.sleep(1)
+        self.spawn(notify)
+
+    def on_user_message(self, payload):
+        payload = json.loads(payload)
+        print "action: %s" % (payload['action'])
+        self.broadcast_event('data', 'command accepted')
+
+
+class Application(object):
+    def __init__(self):
+        self.buffer = []
+
+    def __call__(self, environ, start_response):
+        path = environ['PATH_INFO'].strip('/') or 'index.html'
+
+        if path.startswith('static/') or path == 'index.html':
+            try:
+                data = open(path).read()
+            except Exception:
+                return not_found(start_response)
+
+            if path.endswith(".js"):
+                content_type = "text/javascript"
+            elif path.endswith(".css"):
+                content_type = "text/css"
+            elif path.endswith(".swf"):
+                content_type = "application/x-shockwave-flash"
+            else:
+                content_type = "text/html"
+
+            start_response('200 OK', [('Content-Type', content_type)])
+            return [data]
+
+        if path.startswith("socket.io"):
+            socketio_manage(environ, {'/terminal': EnrouteNamespace})
+        else:
+            return not_found(start_response)
+
+
+def system(cmd):
+    """
+    Invoke a shell command. Primary replacement for os.system calls.
+    """
+    ret = subprocess.Popen(
+        cmd, shell=True, stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+    out, err = ret.communicate()
+    return out, err
+
+
+def hello():
+    return "hello world"
+
+
+def not_found(start_response):
+    start_response('404 Not Found', [])
+    return ['<h1>Not Found</h1>']
+
+
+if __name__ == '__main__':
+    print 'Listening on port http://0.0.0.0:8080 and on port 10843 \
+        (flash policy server)'
+    SocketIOServer(
+        ('0.0.0.0', 8080), Application(),
+        resource="socket.io", policy_server=True,
+        policy_listener=('0.0.0.0', 10843)
+    ).serve_forever()
