e7ba7c31d4e2a7d2875c365db6d04a5014e2e015
Souradeep De <souradeep.2011@gmail.com>
2014-10-24 14:30:09 +0530

    Establish logging in WebTermNamespace

--- namespace.py
+++ namespace.py
@@ -1,27 +1,29 @@
-from socketio.namespace import BaseNamespace
-from socketio.mixins import BroadcastMixin, RoomsMixin
-
 import json
+import logging
 from uuid import uuid4
 from client_thread import ClientThread
 
+from socketio.namespace import BaseNamespace
+from socketio.mixins import BroadcastMixin, RoomsMixin
+
 
 class WebTermNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
-    def __init__(self):
-        BaseNamespace.__init__(self)
-        RoomsMixin.__init__(self)
-        BroadcastMixin.__init__(self)
-
-        # client_threads is used to record the
-        # threads dedicated to the clients
-        self.client_threads = []
+    client_threads = []
+    logging.basicConfig(filename='webterm.log', level=logging.INFO)
 
     def generate_uuid(self):
+        logging.info("Generating UUID")
         return str(uuid4())
 
     def add_new_client(self, UUID):
         new_client_thread = ClientThread(UUID)
+
+        logging.info("Attempting to add new client...")
         self.client_threads.append(new_client_thread)
+        logging.info("New client added.")
+
+        new_client_thread.start()
+        logging.info("Client thread started")
 
     def recv_connect(self):
         '''
@@ -29,6 +31,7 @@ class WebTermNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
         Generates a uuid, assigns it to the client. This uuid is to be used to
         keep track of the thread assigned to the client.
         '''
+        logging.info("New client connection requested")
         new_uuid = self.generate_uuid()
 
         # create a dedicated thread for the client, and add it to the
@@ -40,12 +43,14 @@ class WebTermNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
         }
 
         self.emit('establish-connection', json.dumps(payload))
+        logging.info("UUID sent to client")
 
     def on_user_message(self, payload):
         '''
         Called when a client sends a message to the realtime server
         '''
+        logging.info("New command recieved from a client.")
         payload = json.loads(payload)
         print "Recieved: ", payload['command']
 
-        self.emit('data', json.dumps(response_payload))
+        #self.emit('data', json.dumps(response_payload))
