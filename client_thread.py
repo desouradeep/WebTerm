e6f65deeaa4f9c78f98622036c3f7d6c06435019
Souradeep De <souradeep.2011@gmail.com>
2014-10-24 17:09:58 +0530

    Add helper to determine if a thread is zombied Use a shotgun to splash it's head! :D

--- client_thread.py
+++ client_thread.py
@@ -1,6 +1,11 @@
 import threading
 import sh
 import logging
+from datetime import datetime
+
+# threads which were not used for this zombie_time, will be
+# considered as zombies, and will be killed
+zombie_time = 2 * 60 * 60  # 2 hours
 
 
 class ClientThread(threading.Thread):
@@ -8,11 +13,26 @@ class ClientThread(threading.Thread):
         threading.Thread.__init__(self)
         self.setDaemon(True)
         self.UUID = UUID
+        self.set_last_called()
+
+    def set_last_called(self):
+        self.last_called = datetime.now()
+
+    def isZombie(self):
+        datetime_now = datetime.now()
+        inactice_time = datetime_now - self.last_called
+
+        if inactice_time.seconds > zombie_time:
+            zombied = True
+        else:
+            zombied = False
+        return zombied
 
     def run(self):
         logging.info("Starting thread %s" % (self.UUID))
 
     def execute_command(self, cmd):
+        self.set_last_called()
         output = ''
         error = ''
         cmd = tuple(cmd.split())
